325bf78af646d43eca05e34d9fea2266cab9d86b
Pavanntej <pavanntej@gmail.com>
2026-03-18 17:21:05 +0530

    Update supabase.js

--- public-site/src/supabase.js
+++ public-site/src/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
 export const supabase = createClient(
-  "YOUR_SUPABASE_URL",
-  "YOUR_ANON_KEY"
+  "https://phhflrhgfqmezsiikmim.supabase.co",
+  "sb_publishable_QBNfyTmVYlLcSlv63oeYdA_j0J2HA72"
 )
