1978ad857269a91f152a29fee1351c20ad7d68c1
Pavanntej <pavanntej@gmail.com>
2026-03-18 18:34:51 +0530

    Create supabase.js

---
+++ admin-dashboard/src/supabase.js
@@ -0,0 +1,6 @@
+import { createClient } from '@supabase/supabase-js'
+
+export const supabase = createClient(
+  "https://phhflrhgfqmezsiikmim.supabase.co",
+  "sb_publishable_QBNfyTmVYlLcSlv63oeYdA_j0J2HA72"
+)
